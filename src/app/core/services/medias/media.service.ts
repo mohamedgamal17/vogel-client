56c145fb977b503402681af08379abc7f51331c8
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-06-20 20:03:16 +0300

    Add Media Service

---
+++ src/app/core/services/medias/media.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../../environments/environment";
+import { HttpClient, HttpEvent } from "@angular/common/http";
+import { Media } from "../../types/medias/media.interfcae";
+import { Observable, finalize } from "rxjs";
+
+@Injectable({
+     providedIn: "root"
+})
+
+export default class MediaService {
+     private baseUrl = environment.apiUrl + "/" + "medias"
+
+     constructor(private httpClient: HttpClient) {
+
+     }
+
+     getAll() {
+          return this.httpClient.get<Media[]>(this.baseUrl);
+     }
+
+     getById(id : string){
+          return this.httpClient.get<Media>(this.baseUrl + "/" + id)
+     }
+
+     create(file : File) : Observable<HttpEvent<Media>>{
+          var formData = new FormData();
+          formData.append('file', file)
+          return this.httpClient.post<Media>(this.baseUrl,formData, {
+               reportProgress : true,
+               observe:"events"
+          });
+     }
+}
