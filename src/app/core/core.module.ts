0f9172a5ee0ad1cf1faad3e4990bb07209cbccf6
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-06-23 22:46:51 +0300

    Register services in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import UserService from './services/users/user.service';
+import PostService from './services/posts/post.service';
+import CommentService from './services/comments/comment.service';
+import MediaService from './services/medias/media.service';
+
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+  ],
+  providers :[UserService, PostService, CommentService, MediaService]
 })
 export class CoreModule { }
