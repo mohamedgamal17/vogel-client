4be649b130d836ab1b8cf70dcd593be74a99322d
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-06-25 05:04:11 +0300

    Add Html Template Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [
     provideClientHydration()
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import $ from "jquery";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+
+
+export class HeaderComponent implements AfterViewInit {
+  constructor() {
+  }
+  ngAfterViewInit(): void {
+    $(".msg-trigger-btn").on("click", function (event) {
+      event.stopPropagation();
+      event.preventDefault();
+      var $this = $(this);
+      var $prevTartget = $(this).parent().siblings().children(".msg-trigger-btn").attr('href');
+      var target = $this.attr('href');
+  
+      if(target){
+        $(target).slideToggle();
+      }
+      
+      if($prevTartget){
+        $($prevTartget).slideUp();
+      }
+    });
+  }
+
+
 
 }
