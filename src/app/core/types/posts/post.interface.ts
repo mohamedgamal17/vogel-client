78bdbc10b02c6db5efa2aabd40f0add41109a2e2
Mohamed <Mohamed@DESKTOP-HUTBIV2>
2024-07-31 20:12:01 +0300

    Update post interface

--- src/app/core/types/posts/post.interface.ts
+++ src/app/core/types/posts/post.interface.ts
@@ -6,4 +6,10 @@ export interface Post extends Entity{
      caption : string,
      user : User,
      media? : Media
+     reactionSummary : {
+          totalLike : number,
+          totalLove : number,
+          totalSad : number,
+          totalAngry : number
+     }
 }
